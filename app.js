7e6bd13e068a2a46df745e9ba72b272ce9239566
Kuchar <sz.blazynski@gmail.com>
2021-03-19 15:21:18 +0100

    Add mangodb connection

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require('cors');
 
-app.use(express.json())
+
+require('dotenv/config')
+
+
+app.use(express.json());
+app.use(cors())
 
 app.post('/register', (req, res) => {
-    
+  const username = req.body.username
+  const email = req.body.email
+  const password = req.body.password
+
 })
+
+// Connect to DB
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true },
+  () => console.log("Connected to db")
+);
+
+app.listen(5000, () => console.log('App is listing on 5000'));
---
+++ routes/testAPI.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const registerTemplateCopy = require("../models/RegisterModels");
+
+router.post("/register", (req, res) => {
+  const signUpUser = new registerTemplateCopy({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  signUpUser
+    .save()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((error) => {
+      res.json(error);
+    });
+});
+
+module.exports = router;
