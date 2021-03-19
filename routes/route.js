9a9ac644f99d3b6fb52dd60f314ddefd99f7dd26
Kuchar <sz.blazynski@gmail.com>
2021-03-19 17:22:44 +0100

    Crypt password

--- routes/route.js
+++ routes/route.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const registerTemplateCopy = require("../models/RegisterModels");
+const bcrypt = require("bcrypt")
 
-router.post("/register", (req, res) => {
+router.post("/register",async (req, res) => {
+
+  const saltPassword = await bcrypt.genSalt(10)
+  const securePassword = await bcrypt.hash(req.body.password, saltPassword)
+
   const signUpUser = new registerTemplateCopy({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: securePassword,
   });
 
   signUpUser.save()
